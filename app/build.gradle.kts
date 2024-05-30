2e00de370b706dd05a73dcd09c24093397e5143a
Meida Dela Risyafa Auliya <meidadelaris@student.ub.ac.id>
2024-05-30 22:58:33 +0700

    Profile ItemRecipe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation ("com.google.android.material:material:1.3.0-alpha03")
-
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
