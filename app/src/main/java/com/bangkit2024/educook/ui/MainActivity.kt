bef015c1421d1bead531111b479487122a3292f0
Dito Ardi Pratama <127459191+ditooard@users.noreply.github.com>
2024-05-30 11:46:19 +0700

    bottomnav fix

--- app/src/main/java/com/bangkit2024/educook/ui/MainActivity.kt
+++ app/src/main/java/com/bangkit2024/educook/ui/MainActivity.kt
@@ -1,21 +1,42 @@
 package com.bangkit2024.educook.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.bangkit2024.educook.R
+import com.bangkit2024.educook.ui.nav_activity.BottomFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            onBackPressedMethod()
+        }
+    }
+
+    private fun onBackPressedMethod() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            finish()
+        }
+    }
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.mainFragmentContainer, BottomFragment())
+                .commitNow()
         }
     }
 }
--- app/src/main/java/com/bangkit2024/educook/ui/nav_activity/BottomFragment.kt
+++ app/src/main/java/com/bangkit2024/educook/ui/nav_activity/BottomFragment.kt
@@ -1,66 +1,61 @@
-package com.bangkit2024.educook.fragment.bottom_fragment
+package com.bangkit2024.educook.ui.nav_activity
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.Fragment
 import com.bangkit2024.educook.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
-class MainFragment : Fragment() {
-
+class BottomFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val view =  inflater.inflate(R.layout.fragment_bottom, container, false)
 
         val bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.bottomNavigation)
         bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
+            when(it.itemId){
                 R.id.bottom_home -> {
-                    replaceFragment(HomeFragment())
+                    replaceFragment(HomeActivity())
                     activity?.title = "Home"
                 }
-
                 R.id.bottom_search -> {
-                    replaceFragment(SearchFragment())
+                    replaceFragment(SearchActivity())
                     activity?.title = "Search"
                 }
-
                 R.id.bottom_bookmark -> {
-                    replaceFragment(BookmarkFragment())
-                    activity?.title = "Bookmark"
+                    replaceFragment(BookmarkActivity())
+                    activity?.title = "Boomark"
                 }
-
                 R.id.bottom_profile -> {
-                    replaceFragment(ProfileFragment())
+                    replaceFragment(ProfileActivity())
                     activity?.title = "Profile"
                 }
             }
             true
         }
 
-        replaceFragment(HomeFragment())
+        replaceFragment(HomeActivity())
         activity?.title = "Home"
         bottomNavigationView.selectedItemId = R.id.bottom_home
 
         val addFab = view.findViewById<FloatingActionButton>(R.id.addFabBtn)
         addFab.setOnClickListener {
-            Toast.makeText(context, "Add Clicked", Toast.LENGTH_LONG).show()
+            Toast.makeText(context,"Add Clicked",Toast.LENGTH_LONG).show()
         }
         return view
     }
-
     private fun replaceFragment(fragment: Fragment) {
         parentFragmentManager
             .beginTransaction()
-            .replace(R.id.bottomFragment, fragment)
+            .replace(R.id.bottomFragment,fragment)
             .commit()
     }
 
