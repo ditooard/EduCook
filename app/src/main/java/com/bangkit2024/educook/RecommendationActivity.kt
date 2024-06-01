4696d22685232aa6a1df50e7f45ceff5c5968b61
Meida Dela Risyafa Auliya <meidadelaris@student.ub.ac.id>
2024-06-01 18:09:09 +0700

    Finishing slashing

--- app/src/main/java/com/bangkit2024/educook/RecommendationActivity.kt
+++ app/src/main/java/com/bangkit2024/educook/RecommendationActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class RekomendasiActivity : AppCompatActivity() {
+class RecommendationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_rekomendasi)
+        setContentView(R.layout.activity_recommendation)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/bangkit2024/educook/ui/nav_activity/SearchActivity.kt
+++ app/src/main/java/com/bangkit2024/educook/ui/nav_activity/SearchActivity.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.bangkit2024.educook.R
+import com.bangkit2024.educook.databinding.ActivitySearchBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +24,8 @@ class SearchActivity : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var binding: ActivitySearchBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,9 +37,10 @@ class SearchActivity : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_search, container, false)
+        binding = ActivitySearchBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
